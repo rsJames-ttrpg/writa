69353c18a0425aecaa9bf708e81d93ebd81dc0eb
Jack Mayo <jackomayo@gmail.com/>
2026-04-24 21:18:23 +0100

    feat(writing): add vimtex (requires texlive + zathura installed separately)

---
+++ lua/plugins/writing/vimtex.lua
@@ -0,0 +1,11 @@
+return {
+  "lervag/vimtex",
+  ft = { "tex", "plaintex" },
+  init = function()
+    vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_compiler_method = "latexmk"
+    vim.g.vimtex_quickfix_mode = 0
+    vim.g.vimtex_mappings_enabled = 1
+    vim.g.vimtex_imaps_enabled = 0
+  end,
+}
