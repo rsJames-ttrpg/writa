6a9f9bc122bc1e63b5adf31d6c6c279d46529ad3
Jack Mayo <jackomayo@gmail.com/>
2026-04-24 20:50:03 +0100

    feat(polish): writing_mode autocmd for markdown/tex/fountain/text

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,19 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+return function()
+  local group = vim.api.nvim_create_augroup("writa_writing_mode", { clear = true })
 
--- This will run last in the setup process.
--- This is just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
+  vim.api.nvim_create_autocmd("FileType", {
+    group = group,
+    pattern = { "markdown", "tex", "plaintex", "fountain", "text" },
+    callback = function(args)
+      vim.opt_local.spell = true
+      vim.opt_local.wrap = true
+      vim.opt_local.linebreak = true
+      vim.opt_local.breakindent = true
+      vim.opt_local.conceallevel = 2
+      vim.opt_local.concealcursor = ""
+      if args.match == "fountain" then
+        vim.opt_local.textwidth = 80
+      end
+    end,
+  })
+end
