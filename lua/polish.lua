b45388a4e06bf026f131e1d5cfee2a88c9ae3c95
Jack Mayo <jackomayo@gmail.com/>
2026-04-24 20:53:35 +0100

    fix(polish): run autocmd registration at module load (not via unused returned function)

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,19 +1,17 @@
-return function()
-  local group = vim.api.nvim_create_augroup("writa_writing_mode", { clear = true })
+local group = vim.api.nvim_create_augroup("writa_writing_mode", { clear = true })
 
-  vim.api.nvim_create_autocmd("FileType", {
-    group = group,
-    pattern = { "markdown", "tex", "plaintex", "fountain", "text" },
-    callback = function(args)
-      vim.opt_local.spell = true
-      vim.opt_local.wrap = true
-      vim.opt_local.linebreak = true
-      vim.opt_local.breakindent = true
-      vim.opt_local.conceallevel = 2
-      vim.opt_local.concealcursor = ""
-      if args.match == "fountain" then
-        vim.opt_local.textwidth = 80
-      end
-    end,
-  })
-end
+vim.api.nvim_create_autocmd("FileType", {
+  group = group,
+  pattern = { "markdown", "tex", "plaintex", "fountain", "text" },
+  callback = function(args)
+    vim.opt_local.spell = true
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+    vim.opt_local.breakindent = true
+    vim.opt_local.conceallevel = 2
+    vim.opt_local.concealcursor = ""
+    if args.match == "fountain" then
+      vim.opt_local.textwidth = 80
+    end
+  end,
+})
