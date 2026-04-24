ac60512619b975898840c636ad49457e73b420d8
Jack Mayo <jackomayo@gmail.com/>
2026-04-24 20:46:05 +0100

    feat(mason): install ltex-ls, vale-ls, vale, marksman

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,28 +1,16 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- Customize Mason
-
 ---@type LazySpec
 return {
-  -- use mason-tool-installer for automatically installing Mason packages
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    -- overrides `require("mason-tool-installer").setup(...)`
-    opts = {
-      -- Make sure to use the names found in `:Mason`
-      ensure_installed = {
-        -- install language servers
-        "lua-language-server",
-
-        -- install formatters
-        "stylua",
-
-        -- install debuggers
-        "debugpy",
-
-        -- install any other package
-        "tree-sitter-cli",
-      },
-    },
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
+        "ltex-ls",
+        "vale-ls",
+        "vale",
+        "marksman",
+      })
+      return opts
+    end,
   },
 }
