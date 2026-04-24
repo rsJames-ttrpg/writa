325ae4b870917236e0287d4cc4c008091d7af4b6
Jack Mayo <jackomayo@gmail.com/>
2026-04-24 21:23:16 +0100

    feat(writing): add thesaurus_query

---
+++ lua/plugins/writing/thesaurus.lua
@@ -0,0 +1,7 @@
+return {
+  "Ron89/thesaurus_query.vim",
+  cmd = { "Thesaurus", "ThesaurusQueryReplaceCurrentWord" },
+  keys = {
+    { "<Leader>Ws", "<cmd>ThesaurusQueryReplaceCurrentWord<cr>", desc = "Thesaurus: replace word" },
+  },
+}
